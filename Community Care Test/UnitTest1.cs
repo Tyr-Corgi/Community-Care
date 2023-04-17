f4a84b85f25146e4c06ac9521e920f390757144d
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-04-17 09:35:47 -0600

    started initial framework

---
+++ Community Care Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Community_Care_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Community Care/Methods.cs
@@ -0,0 +1,37 @@
+﻿namespace Community_Care
+{
+    public class Methods
+    {
+        public static bool MedicalSeriveAvailable(string answer1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string NoVAMedicalFacility(string answer2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string DistanceEligibility(string answer3) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string DriveTime(string answer4)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool BestMedicalInterest(string answer5)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void QualityStandards()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
