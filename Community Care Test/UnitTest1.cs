8051f12f352477e38d2d987045ac46afa3ea6d8a
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-04-18 12:37:30 -0600

    completed 3 methods, I can not figure out how to make TDD work

--- Community Care Test/UnitTest1.cs
+++ Community Care Test/UnitTest1.cs
@@ -1,21 +1,21 @@
+using System;
+using Xunit;
+
 namespace Community_Care_Test
 {
-    public class MethodTests
-    {
-        public class MethodTests
-        {
-            [Theory]
-            [InlineData(true, true)]
-            [InlineData(false, false)]
-            public void MedicalSeriveAvailable(bool input, bool expected)
-            {
-                // Arrage
-
-                // Act
-
-                // Assert
-                Assert.
-            }   
-        }
-    }
+//    public class MethodTests
+//    {
+//        [Theory]
+//        [InlineData(true, true)]
+//        [InlineData(false, false)]
+//        public void MedicalSeriveAvailable(bool input, bool expected)
+//        {
+//            // Arrage
+//            MethodTests challenger = new MethodTests();
+//            // Act
+//            bool actual = challenger.MedicalSeriveAvailable(input);
+//            // Assert
+//            Assert.Equal(expected, actual);
+//        }
+//    }
 }
--- Community Care/Methods.cs
+++ Community Care/Methods.cs
@@ -2,19 +2,52 @@
 {
     public class Methods
     {
-        public static bool MedicalSeriveAvailable(string answer1)
+        public static bool MedicalSeriveNOTAvailable(bool answerQ1)
         {
-            throw new NotImplementedException();
+            bool question1Answer = answerQ1;
+
+            if (question1Answer == true)
+            {
+                Console.WriteLine("You may qualify for community care");
+            }
+
+            return question1Answer;
         }
 
-        public static string NoVAMedicalFacility(string answer2)
+        public static bool NoVAMedicalFacility(string veteranState)
         {
-            throw new NotImplementedException();
+            string[] statesWithoutVAMed = { "Alaska", "Hawaii", "New Hampshire", "Guam", "American Samoa", "Northern Mariana Islands", "U.S. Virgin Islands" };
+            bool stateHasVAMed = Array.Exists(statesWithoutVAMed, state => state.Equals(veteranState, StringComparison.OrdinalIgnoreCase));
+
+            if (stateHasVAMed == false)
+            {
+                Console.WriteLine("You may qualify for community care");
+            }
+
+            return stateHasVAMed;
         }
 
-        public static string DistanceEligibility(string answer3) 
-        {
-            throw new NotImplementedException();
+        public static bool DistanceEligibility(double answerQ3)
+        {   
+            var veteranState = "Montana";
+            var vetLocation = answerQ3;
+            var timeAtVetLocation = "06/05/2018";
+            string[] stateswithLowestPop = { "North Dakota", "South Dakota", "Montana", "Alaska", "Wyoming" };
+            bool livesInLowDensity = Array.Exists(stateswithLowestPop, state => state.Equals(veteranState, StringComparison.OrdinalIgnoreCase));
+
+
+
+            // Check if veteran is eligible under the 40-mile criterion and is continuing to reside in a qualifying location
+            if (timeAtVetLocation != "06/06/2018" && vetLocation > 40)
+            {
+                // Check if veteran is residing in one of the lowest population density states or has received care between certain dates and requires care before June 2020
+                if (livesInLowDensity == true)
+                {
+                    Console.WriteLine("You may qualify for community care");
+                }
+            }
+
+            return false;
         }
 
         public static string DriveTime(string answer4)
@@ -31,7 +64,5 @@
         {
             throw new NotImplementedException();
         }
-
-
     }
 }
